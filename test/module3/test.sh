2e8c10ae482f2c6f71d5b622bb6c157111628fa0
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2026-04-05 06:54:49 +0900

    modified:   test/module3/test.sh

--- test/module3/test.sh
+++ test/module3/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ${0%/*}
-../resolver.sh test.se
+if ! ../resolver.sh test.se
+then exit 1
+fi
 script -q /dev/null python3 ../vproc_run.py --trace test.module.se case1 > /tmp/case1.log 2>&1
 diff case1.log /tmp/case1.log
 script -q /dev/null python3 ../vproc_run.py --trace test.module.se case2 > /tmp/case2.log 2>&1
